98a93cefa841e513b6ab04119a846022c1c48a66
Lalit <lalualpha123@gmail.com>
2024-10-15 21:26:10 +0900

    My first JSP servlet Project added

---
+++ src/main/kotlin/com/example/mydemoproject/HelloServlet.kt
@@ -0,0 +1,26 @@
+package com.example.mydemoproject
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = ["/hello-servlet"])
+class HelloServlet : HttpServlet() {
+    private lateinit var message: String
+
+    override fun init() {
+        message = "Hello World!, Clicking the button to start hello servlet"
+    }
+
+    public override fun doGet(request: HttpServletRequest, response: HttpServletResponse) {
+        response.contentType = "text/html"
+
+        // Hello
+        val out = response.writer
+        out.println("<html><body>")
+        out.println("<h1>$message</h1>")
+        out.println("</body></html>")
+    }
+
+    override fun destroy() {
+    }
+}
